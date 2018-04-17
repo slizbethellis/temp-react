220a5b56de56e8517bbdfb0ded267d791b9f38c3
slizbethellis <slizbethellis@gmail.com>
2018-04-17 01:22:59 -0500

    fixed jumping scroll problem when hovering on partial boxes at top of screen

--- src/components/Portfolio/PortfolioItem.js
+++ src/components/Portfolio/PortfolioItem.js
@@ -26,14 +26,16 @@ class PortfolioItem extends Component {
         <div className="portfolio-item" onMouseEnter={this.update} onMouseLeave={this.update}>
           <span className="caption">
             <span className="caption-content">
-              <Animate timeout={400}
-                classNames="btnsFade"
-                shouldShow={this.state.showBtns}>
+              <div className="btn-box">
+                <Animate timeout={400}
+                  classNames="btnsFade"
+                  shouldShow={this.state.showBtns}>
                   <div>
                     {this.props.github ? (<a className={`btn btn-outline-light rounded ${this.state.firstLoad ? "d-none" : ""}`} href={this.props.github} target="_blank" rel='noopener noreferrer'>GitHub <FaCodeFork /></a>) : ""}
                     {this.props.website ? (<a className={`btn btn-outline-light rounded ${this.state.firstLoad ? "d-none" : ""}`} href={this.props.website} target="_blank" rel='noopener noreferrer'>Website <FaChain /></a>) : ""}
                   </div>
-              </Animate>
+                </Animate>
+              </div>
               <h2>{this.props.projectTitle}</h2>
               <p className="mb-0">
                 <small><em>{this.props.technologies}</em></small>
